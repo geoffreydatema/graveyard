d4a92a07f390bdcd23f784eecd6adb00d5a266fd
Geoffrey Datema <geoffrey@geoffreydatema.com>
2024-07-29 16:05:21 -0700

    finished fromBase92() implementation to handle base92 numbers of any placevalue

--- graveyard.py
+++ graveyard.py
@@ -25,13 +25,22 @@ def base92(decimal):
         print("cannot convert negative decimal to base92")
         return None
 
+# def fromBase92(base92):
+#     counter = 0
+#     for digit in BASE92:
+#         if digit == base92:
+#             return(counter)
+#         else:
+#             counter += 1
+
 def fromBase92(base92):
-    counter = 0
-    for digit in BASE92:
-        if digit == base92:
-            return(counter)
-        else:
-            counter += 1
+    base92Decimals = {'~': 0, 'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26, 'a': 27, 'b': 28, 'c': 29, 'd': 30, 'e': 31, 'f': 32, 'g': 33, 'h': 34, 'i': 35, 'j': 36, 'k': 37, 'l': 38, 'm': 39, 'n': 40, 'o': 41, 'p': 42, 'q': 43, 'r': 44, 's': 45, 't': 46, 'u': 47, 'v': 48, 'w': 49, 'x': 50, 'y': 51, 'z': 52, '0': 53, '1': 54, '2': 55, '3': 56, '4': 57, '5': 58, '6': 59, '7': 60, '8': 61, '9': 62, '!': 63, '#': 64, '$': 65, '%': 66, '&': 67, "'": 68, '(': 69, ')': 70, '*': 71, '+': 72, ',': 73, '-': 74, '.': 75, '/': 76, ':': 77, ';': 78, '<': 79, '=': 80, '>': 81, '?': 82, '@': 83, '[': 84, ']': 85, '^': 86, '_': 87, '`': 88, '{': 89, '|': 90, '}': 91}
+    decimal = 0
+    power = 0
+    for char in reversed(base92):
+        decimal += base92Decimals[char] * (92 ** power)
+        power += 1
+    return decimal
 
 def fread(path):
     data = None
@@ -290,6 +299,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     main(args.source, args.translatepython, args.translategraveyard, args.outputtombstone , args.interpret , args.interprettombstone)
 
-# !* fix fromBase92() algorithm to work with numbers over 91
 # !* allow robust token parsing when graveyard code has a mix of mapped and unmapped tokens
 # !* modularize translateToGraveyard functions because there's a lot of repeated code there
